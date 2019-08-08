428cfce4057a2082056e72934d565864c9fc6dd3
Sang_Ha <sanghac19@gmail.com>
2019-08-08 14:57:34 +0900

    profile ui update

--- main/frontend/src/views/Profiles.vue
+++ main/frontend/src/views/Profiles.vue
@@ -2,33 +2,28 @@
   <div class="profile">
     <navbar class="nav" />
     <prof-header class="header" />
-    <porf-body1 class="body1" />
-    <porf-body2 class="body2" />
-    <porf-body3 class="body3" />
+    <prof-body1 class="body1" />
+    <prof-body2 class="body2" />
+    <prof-body3 class="body3" />
+    <prof-footer class="footer" />
   </div>
 </template>
 <script>
-import { mapState } from "vuex";
 import Navbar from "../components/homePage/Navbar.vue";
 import ProfHeader from "../components/profilePage/ProfileHeader.vue";
-import PorfBody1 from "../components/profilePage/ProfileBody.vue";
-import PorfBody2 from "../components/profilePage/ProfileBody2.vue";
-import PorfBody3 from "../components/profilePage/ProfileBody3.vue";
+import ProfBody1 from "../components/profilePage/ProfileBody.vue";
+import ProfBody2 from "../components/profilePage/ProfileBody2.vue";
+import ProfBody3 from "../components/profilePage/ProfileBody3.vue";
+import ProfFooter from "../components/profilePage/ProfileFooter.vue";
 
 export default {
-  computed: {
-    ...mapState(["profile"])
-  },
-  created() {
-    const userId = this.$route.params.user;
-    this.$store.dispatch("getProfileInfo", userId);
-  },
   components: {
     Navbar,
     ProfHeader,
-    PorfBody1,
-    PorfBody2,
-    PorfBody3
+    ProfBody1,
+    ProfBody2,
+    ProfBody3,
+    ProfFooter
   }
 };
 </script>
@@ -36,6 +31,8 @@ export default {
 /* nav */
 
 .nav /deep/ .navhead {
+  padding: 0 1rem;
+  padding-top: 1rem;
   border-bottom: none;
 }
 .nav /deep/ .navhead-icon:hover {
@@ -46,25 +43,40 @@ export default {
 }
 .nav /deep/ .navhead-brand-logo {
   content: url("../assets/home.png");
-  width: 1.8rem;
-  height: 1.8rem;
+  width: 1.5rem;
+  height: 1.5rem;
   margin-left: 0.6rem;
 }
 
 /* layout */
-
+.profile {
+  background: linear-gradient(-45deg, #ea5455, #feb692);
+}
 .header {
+  background-color: transparent;
+  width: 100%;
   height: 28rem;
-  background-color: darkcyan;
 }
 .body1 {
-  background-color: darkgoldenrod;
+  background-color: white;
+  border-bottom: 1px solid rgb(158, 158, 158);
+  width: 100%;
+  height: 8rem;
 }
 .body2 {
   background-color: darkolivegreen;
+  width: 100%;
+  height: 12rem;
 }
 .body3 {
   background-color: darkgrey;
+  width: 100%;
+  height: 12rem;
+}
+.footer {
+  background-color: darkorange;
+  width: ;
+  height: 8rem;
 }
 </style>
 
