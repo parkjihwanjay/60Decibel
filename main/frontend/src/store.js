68a3b71f56ec708781185ccb4ea53c2f94701fbc
ParkJiHwan <cc6656@naver.com>
2019-08-09 17:04:19 +0900

    바로시작 로직 완료

--- main/frontend/src/store.js
+++ main/frontend/src/store.js
@@ -161,6 +161,9 @@ export const store = new Vuex.Store({
             state.answer.associated_symptom_urinary = [];
             state.answer.associated_symptom_others = "";
             state.answer.abdomen_relevant = [];
+        },
+        RESET_RANDOM_USER(state) {
+            state.random_user = {};
         }
     },
     actions: {
@@ -195,20 +198,20 @@ export const store = new Vuex.Store({
         },
         signup(dispatch, signupObj) {
             // login --> 토큰 반환
-            if (this.state.random_user) {
+            if (this.state.random_user.username && this.state.random_user.email && this.state.random_user.password1 && this.state.random_user.password2) {
                 let quickLogin = {};
-                let username = this.state.random_user.username;
-                let password = this.state.random_user.password1;
-                quickLogin["username"] = username;
-                quickLogin["password"] = password;
+
+                quickLogin["username"] = this.state.random_user.username;
+                quickLogin["email"] = this.state.random_user.email;
+                quickLogin["password1"] = this.state.random_user.password1;
+                quickLogin["password2"] = this.state.random_user.password2;
+
                 axios
-                    .post("http://54.180.144.241:8000/api/rest-auth/registration/", this.state.random_user)
-                    // loginObj = {email,password}
+                    .post("http://54.180.144.241:8000/api/rest-auth/registration/", quickLogin)
                     .then(res => {
                         alert("회원가입이 성공적으로 이뤄졌습니다.");
-                        this.dispatch("login", quickLogin);
-                        router.push({ name: "home" });
                         console.log(res);
+                        router.push({ name: "home" });
                         this.state.random_user = {};
                         // commit("RESET_RANDOM_USER");
                     })
@@ -216,8 +219,7 @@ export const store = new Vuex.Store({
                         alert("이메일과 비밀번호를 확인하세요.");
                         // commit("RESET_RANDOM_USER");
                     });
-            }
-            else {
+            } else {
                 axios
                     .post("http://54.180.144.241:8000/api/rest-auth/registration/", signupObj)
                     // loginObj = {email,password}
@@ -235,7 +237,7 @@ export const store = new Vuex.Store({
                     });
             }
         },
-        //유저 세션 유지 function
+
         getMemberInfo({ commit }) {
             //로컬 스토리지에 저장된 토큰을 저장한다.
             let token = localStorage.getItem("access_token");
@@ -311,62 +313,29 @@ export const store = new Vuex.Store({
                 })
                 .catch(error => {
                     console.log(error);
-                })``;
+                });
         },
+        // 지환 : 랜덤 계정을 생성해서 회원가입 폼에 보냄
         start({ commit }) {
             let startObj = {};
             let username = "";
             let possible =
                 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            for (var i = 0; i < 5; i++)
+            for (var i = 0; i < 12; i++)
                 username += possible.charAt(
                     Math.floor(Math.random() * possible.length)
                 );
-            let email = "";
-            for (i = 0; i < 5; i++)
-                email += possible.charAt(Math.floor(Math.random() * possible.length));
-
-
+            let email = username;
             let password1 = "60dbfighithing!!";
             let password2 = password1;
 
             startObj["username"] = username;
             startObj["email"] = email + "@naver.com";
-            startObj["password1"] = password2;
+            startObj["password1"] = password1;
             startObj["password2"] = password2;
             commit("SET_QUICK_START", startObj);
             router.push({ name: "signup" });
-            // axios
-            //     .post("http://54.180.144.241:8000/api/rest-auth/registration/", startObj)
-            //     // loginObj = {email,password}
-            //     .then(res => {
-            //         router.push({ name: "home" });
-            //         console.log(res);
-            //     });
         },
-        // survey(dispatch, survey_data) {
-        //     console.log(survey_data);
-        //     let token = localStorage.getItem("access_token");
-        //     let config = {
-        //         headers: {
-        //             Authorization: "JWT " + token,
-        //             "Content-Type": "application/json"
-        //         }
-        //     };
-        //     axios
-        //         .post("http://54.180.144.241:8000/api/surveys/stomach/", survey_data, config)
-        //         // loginObj = {email,password}
-        //         .then(res => {
-        //             // 접근 성공시, 토큰 값이 반환된다. (실제로는 토큰과 함께 유저 id를 받아온다.)
-        //             // 토큰을 헤더 정보에 포함시켜서 유저 정보를 요청
-
-        //             console.log(res);
-        //         })
-        //         .catch((error) => {
-        //             alert("왜 안될까?");
-        //             console.log(error);
-        //         });
-        // },
         updateProfileInfo(dispatch, update) {
             console.log(update);
             let token = localStorage.getItem("access_token");
@@ -424,6 +393,9 @@ export const store = new Vuex.Store({
                 .catch((error) => {
                     console.log(error);
                 });
+        },
+        resteRandomUser({ commit }) {
+            commit("RESET_RANDOM_USER");
         }
     }
-});
+})
