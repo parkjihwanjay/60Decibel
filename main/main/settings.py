dd62c41f131948ad02446112b3b8fa87381c317f
antlikelion <ignorant_woo@naver.com>
2019-08-19 16:14:15 +0900

    CORS_ORIGIN_WHITELIST수정

--- main/main/settings.py
+++ main/main/settings.py
@@ -107,8 +107,9 @@ DATABASES = {
 }
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:8000"
+    # "http://127.0.0.1:8080",
+    "http://127.0.0.1:8000",
+    "http://localhost:8080"
 ]
 
 CORS_ALLOW_METHODS = (
