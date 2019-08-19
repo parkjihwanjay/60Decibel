685be6a1465e083f64406391b7591aaadb947d50
antlikelion <ignorant_woo@naver.com>
2019-08-19 22:57:03 +0900

    결과 페이지에서 잘못 불러온 값 수정

--- main/frontend/src/components/resultsPage/history.vue
+++ main/frontend/src/components/resultsPage/history.vue
@@ -3,23 +3,23 @@
     <span style="font-size: 1.3em;">사회력 과거력</span>
     <br />
     <br />
-    <p v-if="`${profile.had_checkup}=false`">{{ profile.had_checkup_true }}년 전에 건강검진을 받았습니다.</p>
+    <p v-if="`${stomach.had_checkup}=false`">{{ stomach.had_checkup_true }}년 전에 건강검진을 받았습니다.</p>
     <br />
-    <p>이전에{{ profile.diagnosed_disease }}을 진단받았습니다.</p>
+    <p>이전에{{ stomach.diagnosed_disease }}을 진단받았습니다.</p>
     <br />
-    <p v-if="`${profile.taking_medicine}`">{{ profile.what_medicine }}을 복용중입니다.</p>
+    <p v-if="`${stomach.taking_medicine}`">{{ stomach.what_medicine }}을 복용중입니다.</p>
     <br />
     <p>
-      <span class="s">{{ profile.family_history }}</span>와(과) 같은 가족력이 있습니다.
+      <span class="s">{{ stomach.family_history }}</span>와(과) 같은 가족력이 있습니다.
     </p>
     <br />
-    <p v-if="`${profile.drinking}`">매주 {{ profile.drinking_per_week }}병의 술을 마십니다.</p>
+    <p v-if="`${stomach.drinking}`">매주 {{ stomach.drinking_per_week }}병의 술을 마십니다.</p>
     <br />
     <p
-      v-if="`${profile.smoking}`"
-    >담배를 {{ profile.how_long_smoking }}년 동안 {{ profile.how_long_smoking }}갑씩 피우고 있습니다.</p>
+      v-if="`${stomach.smoking}`"
+    >담배를 {{ stomach.how_long_smoking }}년 동안 {{ stomach.how_long_smoking }}갑씩 피우고 있습니다.</p>
     <br />
-    <p v-if="`${profile.relevant_data}`">기타 특이사항 : {{ profile.relevant_data }}</p>
+    <p v-if="`${stomach.relevant_data}`">기타 특이사항 : {{ stomach.relevant_data }}</p>
   </div>
 </template>
 
@@ -28,14 +28,11 @@ import { mapState, mapActions } from "vuex";
 import { store } from "../../store.js";
 export default {
   computed: {
-    ...mapState(["profile"])
-  },
-  method: {
-    ...mapActions(["getProfileInfo"])
+    ...mapState(["stomach"])
   },
   created() {
-    const userId = this.$route.params.user;
-    this.$store.dispatch("getProfileInfo", userId);
+    const stomachId = this.$route.params.id;
+    this.$store.dispatch("getStomachInfo", stomachId);
   }
 };
 </script>
