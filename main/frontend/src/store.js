acee90d726dbe8e72d7cd16856c22bad14f8c653
antlikelion <ignorant_woo@naver.com>
2019-08-19 12:29:51 +0900

    프로필 업데이트 다중선택 관련 오류 해결

--- main/frontend/src/store.js
+++ main/frontend/src/store.js
@@ -381,7 +381,7 @@ export const store = new Vuex.Store({
                     });
                 })
                 .catch(error => {
-                    console.log(error);
+                    console.log(error.response.data);
                 });
         },
         setSurveyData1({
